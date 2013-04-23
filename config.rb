d592e680668ce03291d9dbd43d34189cbded27d2
Mark Reilly <mark@alienresident.net>
2013-04-23 15:46:34 -0400

    Added new Sass structure and extensions

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-require 'ninesixty'    
+require 'toolkit'   
 
 # Set this to the root of your project when deployed:
 http_path = "/mediacommons/sites/mediacommons.futureofthebook.org/themes/mediacommons_base"
