2cfa773ced108f405ca14b5766111fba7f497396
gabrielbach <gabriel@curiousprocess.com>
2013-05-16 14:18:10 -0400

    radius mixins, rationalizing variables, general cleaning of css, login-out buttons

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -23,12 +23,21 @@
   <div id="skip-link"><a href="#main-content" id="skip-to-main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a></div>
   <section class="mediacommons-network">
 		<ul class="desktop">
-			<li><a href="index.html"><img src="<?php echo $GLOBALS['base_url'].'/sites/mediacommons.futureofthebook.org/files/mediacommons_new.png' ?>"></a></li>
-			<li><a href="imr-post.html">In Media Res</a></li>
+			<li><a href="#"><img src="<?php echo $GLOBALS['base_url'].'/sites/mediacommons.futureofthebook.org/files/mediacommons_new.png' ?>"></a></li>
+			<li><a href="#">In Media Res</a></li>
 			<li><a href="#">MediaCommons Press</a></li>
-			<li><a href="tne-multi.html" class="active">The New Everyday</a></li>
-			<li><a href="alt-search.html">#Alt-Academy</a></li>
-			<li class="login"><a href="http://dev-dl-pa.home.nyu.edu/mediacommons/user/login">Login</a></li>
+			<li><a href="#" class="active">The New Everyday</a></li>
+			<li><a href="#">#Alt-Academy</a></li>
+
+			<?php
+			if ($user->uid > 0) {
+			    print '<li class="login">' . l(t('logout'), 'logout'). '</li>';
+			}
+			else {
+			    print '<li class="login">' . l(t('login'), 'user'). '</li>';
+			}
+			?>
+
 		</ul>
 		<ul class="mobile">
 			<li><a href=""><img src="<?php echo $GLOBALS['base_url'].'/sites/mediacommons.futureofthebook.org/files/mediacommons_new.png' ?>"></a></li>
