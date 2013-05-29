23c11871ed640a26c562182527226389ae895226
Mark Reilly <mark@alienresident.net>
2013-05-29 16:39:24 -0400

    Signed-off-by: Mark Reilly <mark@alienresident.net>

--- templates/field--field_spokes.tpl.php
+++ templates/field--field_spokes.tpl.php
@@ -1,6 +1,7 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <h2>Posts</h2>
-  <ul class="spokes">
+  <h4>Articles</h4>
+	<ul class="spokes">
     <?php foreach ($items as $delta => $item) : ?>
       <li><?php print render($item); ?></li>
     <?php endforeach; ?>
