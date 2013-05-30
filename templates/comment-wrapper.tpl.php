4954c6efa01434fa617a1905fe4d90dd4adc75fc
gabrielbach <gabriel@curiousprocess.com>
2013-05-30 15:15:32 -0400

    merge clean-up h2 to h4 in comments

--- templates/comment-wrapper.tpl.php
+++ templates/comment-wrapper.tpl.php
@@ -1,7 +1,7 @@
 <div id="comments" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if ($content['comments'] && $node->type != 'forum'): ?>
     <?php print render($title_prefix); ?>
-    <h2 class="title"><?php print t('Comments'); ?></h2>
+    <h4 class="title"><?php print t('Comments'); ?></h4>
     <?php print render($title_suffix); ?>
   <?php endif; ?>
 
