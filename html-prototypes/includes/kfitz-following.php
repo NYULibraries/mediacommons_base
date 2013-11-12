2fddff7e47dc70df7fca1630bc155796934f3265
Laura Henze <laurahenze@gmail.com>
2013-11-12 16:24:49 -0500

    markup and styles for profile pages -- mobile view

--- html-prototypes/includes/kfitz-following.php
+++ html-prototypes/includes/kfitz-following.php
@@ -1,4 +1,4 @@
-<div class="content">
+
   <div class="profile-friends">
     <div class="profile-friend">
       <div class="profile-friend-picture"><a href="/users/jason-mittell"><img src="http://www.gravatar.com/avatar/a3a5068acc5afa035f256d03307eb1a1?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a></div>
@@ -12,9 +12,7 @@
     <div class="profile-friend">
       <div class="profile-friend-picture"><a href="/users/jeremy-butler"><img src="http://www.gravatar.com/avatar/df1e7249db2d2e7a546f6c2be3ef2ac8?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a></div>
       <a href="/users/jeremy-butler">Jeremy Butler</a></div>
-    <div class="profile-friend">
-      <div class="profile-friend-picture"><a href="/users/hoffman"><img src="http://mediacommons.futureofthebook.org/files/pictures/picture-407.jpg"></a></div>
-      <a href="/users/hoffman">hoffman</a></div>
+
     <div class="profile-friend">
       <div class="profile-friend-picture"><a href="/users/tedfriedman"><img src="http://mediacommons.futureofthebook.org/files/pictures/picture-1590.jpg"></a></div>
       <a href="/users/tedfriedman">tedfriedman</a></div>
@@ -22,4 +20,4 @@
       <div class="profile-friend-picture"><a href="/users/nowviskie"><img src="http://www.gravatar.com/avatar/834ca18d78d323690ea57fad3cb0eb63?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a></div>
       <a href="/users/nowviskie">nowviskie</a></div>
   </div>
-</div>
+
