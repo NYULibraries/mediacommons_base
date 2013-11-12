a2c3e4008d6d534b2f749e86cedc35277bd37c46
Mark Reilly <mark@alienresident.net>
2013-11-12 11:45:04 -0500

    updated the viewport meta tag and the google fonts
    Remove some double title tags too

--- html-prototypes/globalnav.php
+++ html-prototypes/globalnav.php
@@ -3,24 +3,24 @@
 <head>
 <meta charset="utf-8">
 <title>In Media Res Sample Post | Navigation Demo</title>
-<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700|Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-<script type="text/javascript">
-      WebFontConfig = {
-        google: { families: [ 'Droid+Sans:400,700:latin', 'Droid+Serif:400,700,400italic,700italic:latin' ] }
-      };
-      (function() {
-        var wf = document.createElement('script');
-        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-          '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-        wf.type = 'text/javascript';
-        wf.async = 'true';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(wf, s);
-      })(); 
-</script>
-<link rel="stylesheet" href="../css/mediacommons_base.css">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-
+  <!-- Using Google Web Font Loader to load fonts instead of Stylesheet -->
+  <!--link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700|Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'--> 
+  <script type="text/javascript">
+    WebFontConfig = {
+      google: { families: [ 'Droid+Sans:400,700:latin', 'Droid+Serif:400,700,400italic,700italic:latin' ] }
+    };
+    (function() {
+      var wf = document.createElement('script');
+      wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+      wf.type = 'text/javascript';
+      wf.async = 'true';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(wf, s);
+    })(); 
+  </script>
+  <link rel="stylesheet" href="../css/mediacommons_base.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 <?php include "includes/header-imr.inc"; ?>
