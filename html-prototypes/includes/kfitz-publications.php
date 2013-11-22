ee2459f0ff0bb2b4fc85d4acd4695777ee55a603
Mark Reilly <mark@alienresident.net>
2013-11-22 15:15:35 -0500

    Fixed links

--- html-prototypes/includes/kfitz-publications.php
+++ html-prototypes/includes/kfitz-publications.php
@@ -1,34 +1,35 @@
+<h2>Publications</h2>
 <div class="content">
   <ul>
     <li class="views-row views-row-1 views-row-odd views-row-first">
-      <div class="biblio-entry"> <a href="/content/planned-obsolescence-publishing-technology-and-future-academy">Planned Obsolescence: Publishing, Technology, and the Future of the Academy</a>. MediaCommons; 2009. </div>
+      <div class="biblio-entry"> <a href="http://mediacommons.futureofthebook.org/content/planned-obsolescence-publishing-technology-and-future-academy">Planned Obsolescence: Publishing, Technology, and the Future of the Academy</a>. MediaCommons; 2009. </div>
     </li>
     <li class="views-row views-row-2 views-row-even">
-      <div class="biblio-entry"><a href="/content/peer-peer-review-and-future-scholarly-authority">Peer-to-Peer Review and the Future of Scholarly Authority</a>. Cinema Journal. 2009;48:124-29. </div>
+      <div class="biblio-entry"><a href="http://mediacommons.futureofthebook.org/content/peer-peer-review-and-future-scholarly-authority">Peer-to-Peer Review and the Future of Scholarly Authority</a>. Cinema Journal. 2009;48:124-29. </div>
     </li>
     <li class="views-row views-row-3 views-row-odd">
-      <div class="biblio-entry"> <a href="/content/memoriam">In Memoriam</a>. Modernism/Modernity. 2009;16:5-6. </div>
+      <div class="biblio-entry"> <a href="http://mediacommons.futureofthebook.org/content/memoriam">In Memoriam</a>. Modernism/Modernity. 2009;16:5-6. </div>
     </li>
     <li class="views-row views-row-4 views-row-even">
-      <div class="biblio-entry"> <a href="/content/teaching-infinite-jest-0">On Teaching Infinite Jest</a>.; 2009.</div>
+      <div class="biblio-entry"> <a href="http://mediacommons.futureofthebook.org/content/teaching-infinite-jest-0">On Teaching Infinite Jest</a>.; 2009.</div>
     </li>
     <li class="views-row views-row-5 views-row-odd">
-      <div class="biblio-entry"><a href="/content/obsolescence">Obsolescence</a>. PMLA. 2008;123(3):718-22. </div>
+      <div class="biblio-entry"><a href="http://mediacommons.futureofthebook.org/content/obsolescence">Obsolescence</a>. PMLA. 2008;123(3):718-22. </div>
     </li>
     <li class="views-row views-row-6 views-row-even">
-      <div class="biblio-entry"> <a href="/content/scholarly-collaboration-digital-age-0">Scholarly Collaboration in the Digital Age</a></span>.; 2008. </div>
+      <div class="biblio-entry"> <a href="http://mediacommons.futureofthebook.org/content/scholarly-collaboration-digital-age-0">Scholarly Collaboration in the Digital Age</a></span>.; 2008. </div>
     </li>
     <li class="views-row views-row-7 views-row-odd">
-      <div class="biblio-entry"><a href="/content/scholarly-collaboration-digital-age">Scholarly Collaboration in the Digital Age</a>.; 2008. </div>
+      <div class="biblio-entry"><a href="http://mediacommons.futureofthebook.org/content/scholarly-collaboration-digital-age">Scholarly Collaboration in the Digital Age</a>.; 2008. </div>
     </li>
     <li class="views-row views-row-8 views-row-even">
-      <div class="biblio-entry"> <a href="/content/future-writing-take-two">Future Writing, Take Two</a>.; 2008. </div>
+      <div class="biblio-entry"> <a href="http://mediacommons.futureofthebook.org/content/future-writing-take-two">Future Writing, Take Two</a>.; 2008. </div>
     </li>
     <li class="views-row views-row-9 views-row-odd">
-      <div class="biblio-entry"><a href="/content/bolter-principle">The Bolter Principle</a>.; 2008. </div>
+      <div class="biblio-entry"><a href="http://mediacommons.futureofthebook.org/content/bolter-principle">The Bolter Principle</a>.; 2008. </div>
     </li>
     <li class="views-row views-row-10 views-row-even views-row-last">
-      <div class="biblio-entry"><a href="/content/mediacommons-scholarly-publishing-age-internet">MediaCommons: Scholarly Publishing in the Age of the Internet</a>. MediaCommons. 2007. </div>
+      <div class="biblio-entry"><a href="http://mediacommons.futureofthebook.org/content/mediacommons-scholarly-publishing-age-internet">MediaCommons: Scholarly Publishing in the Age of the Internet</a>. MediaCommons. 2007. </div>
     </li>
   </ul>
 </div>
