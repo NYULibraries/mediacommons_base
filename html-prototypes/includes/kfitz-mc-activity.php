47aa09d88554b83ab7e157a84d4744b494b1e93e
Mark Reilly <mark@alienresident.net>
2013-11-22 17:15:36 -0500

    Moved things around
    Moved kfitz-comments.php and posts to kfitz-imr-activity.php
    Moved kfitz-mcactivity.php to kfitz-mc-activity.php

---
+++ html-prototypes/includes/kfitz-mc-activity.php
@@ -0,0 +1,30 @@
+<!--THIS IS THE VIEW 'user_recent_posts'-->
+<section class="view mc-activity view-user-recent-posts view-id-user_recent_posts view-display-id-block view-dom-id-2 view-content">
+  <h1>MediaCommons Activity</h1>
+  <ul class="posts mc-posts">
+    <li >
+      <a class="title" href="http://mediacommons.futureofthebook.org/content/what-happened">What Happened?</a>
+      <time datetime="2013-04-26T06:17+08:00">April 26, 2013 – 06:17</time>
+    </li>
+    <li >
+      <a class="title" href="http://mediacommons.futureofthebook.org/content/born-digital">Born Digital</a>
+      <time datetime="2013-01-04T09:31+08:00">January 04, 2013 – 09:31</time>
+    </li>
+    <li >
+      <a class="title" href="http://mediacommons.futureofthebook.org/content/fit-purpose-developing-business-cases-new-services-research-libraries">Fit for Purpose: Developing Business Cases for New Services in Research Libraries</a>
+      <time datetime="2012-10-18T12:11+08:00">October 18, 2012 – 12:11</time>
+    </li>
+    <li >
+      <a class="title" href="http://mediacommons.futureofthebook.org/blog/2012/08/07/rough-cuts-media-and-design-process">Rough Cuts: Media and Design in Process</a>
+      <time datetime="2012-08-07T09:55+08:00">August 07, 2012 – 09:55</time>
+    </li>
+    <li >
+      <a class="title" href="http://mediacommons.futureofthebook.org/blog/2012/06/18/open-review-study-contexts-and-practices">Open Review: A Study of Contexts and Practices</a>
+      <time datetime="2012-06-18T13:11+08:00">June 18, 2012 – 13:11</time>
+    </li>
+    <li >
+      <a class="title" href="http://mediacommons.futureofthebook.org/blog/2012/03/28/complex-television">Complex Television</a>
+      <time datetime="2012-03-28T14:09+08:00">March 28, 2012 – 14:09</span> </time>
+    </li>
+  </ul>
+</section>
