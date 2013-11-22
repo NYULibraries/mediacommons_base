f033952ca718027eec4ba295d87523ca037738c8
Mark Reilly <mark@alienresident.net>
2013-11-22 17:20:17 -0500

    Cleaned up semantics
    Added microformats and rel="me"
    Fixed Links and Post dates

--- html-prototypes/includes/main-profile.php
+++ html-prototypes/includes/main-profile.php
@@ -1,63 +1,44 @@
 <article class="profile">
   <main role="main" >
-  <header>
-    <div class="photo"><img src = "kathleenFitzPatrick.jpg" ></div>
-    <div id="profile-info">
-    <h1>Kathleen FitzPatrick</h1>
-    <div>Director of Scholarly Communication @ Modern Language Association</div>
-    <div>New York, NY, USA</div></div>
-  </header>
-  <section id="contact-info">
-    <div class="button icon-email"><span><a mailto:"adfsdf">oo@example.net</a></span></div>
-    <div class="button icon-www"><span><a href="#">http://www.example.net</a></span></div>
-    <div class="button icon-phone"><span>917.555.1234</span></div>
-    <div class="button icon-skype"><span>kfitz</span></div>     
-  </section>
-  <section >
-    <h2 >MediaCommons Activity</h2>
-    <?php include "includes/kfitz-mcactivity.php"; ?>
- 
-    <h2 >In MediaRes Activity</h2>
-    <h3>Posts</h3>
-    <ul class="mediares-posts">
-      <li>
-        <div class="title"> <a href="/imr/2009/12/02/possible-or-probable-imagined-future-book">"Possible or Probable?": An Imagined Future of the Book</a> </div>
-        <div class="date">3 years 48 weeks ago</div>
-      </li>
-      <li>
-        <div class="title"> <a href="/imr/2007/07/30/its-our-job-to-know-stuff-the-epistemology-of-csi">“It’s Our Job to Know Stuff”: The Epistemology of CSI</a> </div>
-        <div class="date">3 years 51 weeks ago</div>
-      </li>
-      <li>
-        <div class="title"> <a href="/imr/2009/01/29/education-20">Education 2.0?</a> </div>
-        <div class="date">3 years 51 weeks ago</div>
-      </li>
+  <header class="h-card vcard">
+    <img src="kathleenFitzPatrick.jpg" alt="Kathleen FitzPatrick" class="u-photo photo">
+    <h1 class="p-name fn">Kathleen FitzPatrick</h1>
+    <span class="p-job-title title">Director of Scholarly Communication</span> <span class="p-org org">Modern Language Association</span>
+    <span class="h-adr"><span class="p-locality">New York, </span><span class="p-region">NY, </span><span class="p-country-name">USA</span></span>
+    <ul class="contact-info">
+      <li><a class="u-email email" href="mailto:oo@example.net" rel="me"><span>oo@example.net</span></a></li>
+      <li><a class="u-url url www" href="http://www.example.net" rel="external me"><span>www.example.net</span></a></li>
+      <li><a class="p-tel tel" href="tel:+19175551234" rel="me"><span>917.555.1234</span></a></li>
+      <li><a class="u-url url twitter" href="https://twitter.com/kfitz" rel="me"><span>@kfitz</span></a></li>
+      <li><a class="u-url skype" a href="skype:kfitz" rel="me"><span>kfitz</span></a></li>
     </ul>
-    <h3>Comments</h3>
-    <?php include "includes/kfitz-comments.php"; ?>
+  </header>
+    
+    <?php include "includes/kfitz-mc-activity.php"; ?>
+ 
+   
+    
+    <?php include "includes/kfitz-imr-activity.php"; ?>
 
-    <h2>Publications</h2>
-    <?php include "includes/kfitz-publications.php"; ?>
-  </section>
-  <aside>
+    
+    <!--?php include "includes/kfitz-publications.php"; ?-->
+  <aside class="profile-aside research-interests">
     <h2>Research Interests</h2>
-    <div class="content">
       <ul class="Tags">
-        <li><a href="/category/tags/copyright">copyright</a></li>
-        <li><a href="/category/tags/digital-humanities">digital humanities</a></li>
-        <li><a href="/category/tags/digital-publishing">digital publishing</a></li>
-        <li><a href="/category/tags/narrative">Narrative</a></li>
-        <li><a href="/category/tags/obsolescence">obsolescence</a></li>
-        <li><a href="/category/tags/open-access">open access</a></li>
-        <li><a href="/category/tags/peer-peer-review">peer to peer review</a></li>
-        <li><a href="/category/tags/scholarly-publishing">scholarly publishing</a></li>
-        <li><a href="/category/tags/social-networks">Social Networks</a></li>
+        <li><a href="http://mediacommons.futureofthebook.org/category/tags/copyright" rel="tag">copyright</a></li>
+        <li><a href="http://mediacommons.futureofthebook.org/category/tags/digital-humanities" rel="tag">digital humanities</a></li>
+        <li><a href="http://mediacommons.futureofthebook.org/category/tags/digital-publishing" rel="tag">digital publishing</a></li>
+        <li><a href="http://mediacommons.futureofthebook.org/category/tags/narrative" rel="tag">Narrative</a></li>
+        <li><a href="http://mediacommons.futureofthebook.org/category/tags/obsolescence" rel="tag">obsolescence</a></li>
+        <li><a href="http://mediacommons.futureofthebook.org/category/tags/open-access" rel="tag">open access</a></li>
+        <li><a href="http://mediacommons.futureofthebook.org/category/tags/peer-peer-review" rel="tag">peer to peer review</a></li>
+        <li><a href="http://mediacommons.futureofthebook.org/category/tags/scholarly-publishing" rel="tag">scholarly publishing</a></li>
+        <li><a href="http://mediacommons.futureofthebook.org/category/tags/social-networks" rel="tag">Social Networks</a></li>
       </ul>
-    </div>
   </aside>
  
-  <aside id="following">
-    <h2>Following</h2>
+  <aside id="following"  class="profile-aside following">
     <?php include "includes/kfitz-following.php"; ?>
   </aside>
-</main></article>
+</main>
+</article>
