5c95241f63b6ae63202f9d1ce9c9ccbcdd0ff862
Mark Reilly <mark@alienresident.net>
2013-12-03 12:38:55 -0500

    Added h-card and vcard, and rel info

--- html-prototypes/includes/kfitz-following.php
+++ html-prototypes/includes/kfitz-following.php
@@ -1,28 +1,28 @@
 <h2>Following</h2>
-  <ul class="profile-friends content">
-    <li class="profile friend">
-      <a class="profile-friend-picture" href="http://mediacommons.futureofthebook.org/users/jason-mittell"><img src="http://www.gravatar.com/avatar/a3a5068acc5afa035f256d03307eb1a1?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a>
-      <a href="http://mediacommons.futureofthebook.org/users/jason-mittell" rel="colleague">Jason Mittell</a>
+  <ul class="following">
+    <li class="h-card vcard">
+      <a class="u-url" href="http://mediacommons.futureofthebook.org/users/jason-mittell"><img class="u-photo" src="http://www.gravatar.com/avatar/a3a5068acc5afa035f256d03307eb1a1?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a>
+      <a href="http://mediacommons.futureofthebook.org/users/jason-mittell" rel="colleague" class="p-name fn u-url url">Jason Mittell</a>
     </li>
-    <li class="profile friend">
-      <a class="profile-friend-picture" href="http://mediacommons.futureofthebook.org/users/chuck-tryon"><img src="http://www.gravatar.com/avatar/50b10d08d37d7239591873f0712b1e38?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a>
-      <a href="http://mediacommons.futureofthebook.org/users/chuck-tryon" rel="colleague">Chuck Tryon</a>
+    <li class="h-card vcard">
+      <a class="u-url" href="http://mediacommons.futureofthebook.org/users/chuck-tryon"><img class="u-photo" src="http://www.gravatar.com/avatar/50b10d08d37d7239591873f0712b1e38?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a>
+      <a href="http://mediacommons.futureofthebook.org/users/chuck-tryon" rel="colleague" class="p-name fn u-url url">Chuck Tryon</a>
     </li>
-    <li class="profile friend">
-      <a class="profile-friend-picture" href="http://mediacommons.futureofthebook.org/users/avi-santo"><img src="http://mediacommons.futureofthebook.org/files/pictures/picture-5.jpg"></a>
-      <a href="http://mediacommons.futureofthebook.org/users/avi-santo" rel="colleague">Avi Santo</a>
+    <li class="h-card vcard">
+      <a class="u-url" href="http://mediacommons.futureofthebook.org/users/avi-santo"><img class="u-photo" src="http://mediacommons.futureofthebook.org/files/pictures/picture-5.jpg"></a>
+      <a href="http://mediacommons.futureofthebook.org/users/avi-santo" rel="colleague" class="p-name fn u-url url">Avi Santo</a>
     </li>
-    <li class="profile friend">
-      <a class="profile-friend-picture" href="http://mediacommons.futureofthebook.org/users/jeremy-butler"><img src="http://www.gravatar.com/avatar/df1e7249db2d2e7a546f6c2be3ef2ac8?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a>
-      <a href="http://mediacommons.futureofthebook.org/users/jeremy-butler" rel="colleague">Jeremy Butler</a>
+    <li class="h-card vcard">
+      <a class="u-url" href="http://mediacommons.futureofthebook.org/users/jeremy-butler"><img class="u-photo" src="http://www.gravatar.com/avatar/df1e7249db2d2e7a546f6c2be3ef2ac8?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a>
+      <a href="http://mediacommons.futureofthebook.org/users/jeremy-butler" rel="colleague" class="p-name fn u-url url">Jeremy Butler</a>
     </li>
-    <li class="profile friend">
-      <a class="profile-friend-picture" href="http://mediacommons.futureofthebook.org/users/tedfriedman"><img src="http://mediacommons.futureofthebook.org/files/pictures/picture-1590.jpg"></a>
-      <a href="http://mediacommons.futureofthebook.org/users/tedfriedman" rel="colleague">Ted Friedman</a>
+    <li class="h-card vcard">
+      <a class="u-url" href="http://mediacommons.futureofthebook.org/users/tedfriedman"><img class="u-photo" src="http://mediacommons.futureofthebook.org/files/pictures/picture-1590.jpg"></a>
+      <a href="http://mediacommons.futureofthebook.org/users/tedfriedman" rel="colleague" class="p-name fn u-url url">Ted Friedman</a>
     </li>
-    <li class="profile friend">
-      <a class="profile-friend-picture" href="http://mediacommons.futureofthebook.org/users/nowviskie"><img src="http://www.gravatar.com/avatar/834ca18d78d323690ea57fad3cb0eb63?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a>
-      <a href="http://mediacommons.futureofthebook.org/users/nowviskie" rel="colleague">Bethany Nowviskie</a>
+    <li class="h-card vcard">
+      <a class="u-url" href="http://mediacommons.futureofthebook.org/users/nowviskie"><img class="u-photo" src="http://www.gravatar.com/avatar/834ca18d78d323690ea57fad3cb0eb63?d=http%3A%2F%2Fmediacommons.futureofthebook.org%2Fsites%2Fmediacommons.futureofthebook.org%2Fmodules%2Fgravatar%2Favatar.png&amp;s=85&amp;r=G"></a>
+      <a href="http://mediacommons.futureofthebook.org/users/nowviskie" rel="colleague" class="p-name fn u-url url">Bethany Nowviskie</a>
     </li>
   </ul>
 
